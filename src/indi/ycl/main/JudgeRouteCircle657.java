d6e0228098d4009784169d3173e1817fc912b387
hupochuan <1434296509@qq.com>
2017-12-20 09:27:03 +0800

    都比较简单，有一个递归相关的，需要多注意null判断

---
+++ src/indi/ycl/main/JudgeRouteCircle657.java
@@ -0,0 +1,41 @@
+package indi.ycl.main;
+
+public class JudgeRouteCircle657 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new JudgeRouteCircle657().judgeCircle("LL"));
+
+	}
+
+	public boolean judgeCircle(String moves) {
+		int x=0,y=0;
+		char[] command=moves.toCharArray();
+		for (int i = 0; i < command.length; i++) {
+			
+			if(command[i]=='L'){
+				x++;
+				
+			}else if(command[i]=='R'){
+				x--;
+				
+			}else if(command[i]=='U'){
+				y++;
+				
+			}else if(command[i]=='D'){
+				y--;
+				
+			}else{
+				return false;
+			}
+		}
+		if(x==0&&y==0){
+			return true;
+			
+		}else{
+			return false;
+		}
+
+	}
+
+}
