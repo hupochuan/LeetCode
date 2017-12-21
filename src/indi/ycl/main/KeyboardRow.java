7e96392b591bad60596d7150d9ab5c1d6d9bfa37
hupochuan <1434296509@qq.com>
2017-12-21 18:14:12 +0800

    简单题

---
+++ src/indi/ycl/main/KeyboardRow.java
@@ -0,0 +1,73 @@
+package indi.ycl.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeyboardRow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String[] findWords(String[] words) {
+        List<String> result=new ArrayList<String>();
+		//0-9 第一行 10-18 二 19-25
+		String rol1="qwertyuiopQWERTYUIOP";
+		String rol2="asdfghjklASDFGHJKL";
+		String rol3="zxcvbnmZXCVBNM";
+		for (int i = 0; i < words.length; i++) {
+			String str=words[i];
+			int rolnum=0;
+			Boolean tag=true;
+			for (int j = 0; j < str.length(); j++) {
+				String chr=String.valueOf(str.charAt(j));
+				if(rol1.contains(chr)){
+					if(rolnum!=1){
+						if(rolnum==0){
+							rolnum=1;
+						}else{
+							tag=false;
+							break;
+						}
+					}
+					
+				}else if(rol2.contains(chr)){
+					if(rolnum!=2){
+						if(rolnum==0){
+							rolnum=2;
+						}else{
+							tag=false;
+							break;
+						}
+					}
+					
+					
+				}else if(rol3.contains(chr)){
+					if(rolnum!=3){
+						if(rolnum==0){
+							rolnum=3;
+						}else{
+							tag=false;
+							break;
+						}
+					}
+					
+				}
+			}
+			
+			if(tag){
+				result.add(str);
+			}
+			
+			
+		}
+		String[] jieguo=new String[result.size()];
+		for (int i = 0; i < result.size(); i++) {
+			jieguo[i]=result.get(i);
+		}
+		return jieguo;
+
+	}
+
+}
---
+++ src/indi/ycl/main/ReverseString.java
@@ -0,0 +1,15 @@
+package indi.ycl.main;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String reverseString(String s) {
+		return new StringBuilder(s).reverse().toString();
+
+	}
+
+}
