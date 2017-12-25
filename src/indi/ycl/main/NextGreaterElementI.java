3bc222197edf21087d0c7f2bd5ea8a629f163b11
hupochuan <1434296509@qq.com>
2017-12-25 14:44:35 +0800

    简单题

---
+++ src/indi/ycl/main/NextGreaterElementI.java
@@ -0,0 +1,57 @@
+package indi.ycl.main;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.print.attribute.HashDocAttributeSet;
+
+public class NextGreaterElementI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+		int[] result=new int[nums1.length];
+		for (int i = 0; i < nums1.length; i++) {
+			boolean tag=false;
+			int bigernum=-1;
+			for (int j = 0; j < nums2.length; j++) {
+				if(!tag){
+					if(nums1[i]==nums2[j])
+					tag=true;
+				}
+				if(tag){
+					if(nums2[j]>nums1[i]){
+						bigernum=nums2[j];
+					    break;
+					}
+						
+				}
+				
+			}
+			result[i]=bigernum;
+		}
+		return result;
+
+	}
+	public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+		Stack<Integer> stack=new Stack<Integer>();
+		for (int num : nums2) {
+			while(!stack.isEmpty()&&stack.peek()<num)
+				map.put(stack.pop(), num);
+			stack.push(num);
+		}
+		for (int i = 0; i < nums1.length; i++) {
+			nums1[i]=map.getOrDefault(nums1[i], -1);
+		}
+		return nums1;
+		
+		
+	}
+	
+
+}
