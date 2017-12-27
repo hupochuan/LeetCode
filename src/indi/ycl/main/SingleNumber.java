3df47aa8f706fbcf3308afcf9b21af1a76fc6070
hupochuan <1434296509@qq.com>
2017-12-27 11:05:47 +0800

    位运算，方法很巧妙，异或运算

---
+++ src/indi/ycl/main/SingleNumber.java
@@ -0,0 +1,18 @@
+package indi.ycl.main;
+
+public class SingleNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int singleNumber(int[] nums) {
+		for (int i = 1; i < nums.length; i++) {
+			nums[0]^=nums[i];
+		}
+		return nums[0];
+
+	}
+
+}
