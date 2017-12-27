6b994ac41539073a06ed0f6335f9743322cb21ee
hupochuan <1434296509@qq.com>
2017-12-27 10:57:08 +0800

    两个Model

---
+++ src/indi/ycl/model/Employee.java
@@ -0,0 +1,14 @@
+package indi.ycl.model;
+
+import java.util.List;
+
+public class Employee {
+	// It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+
+}
---
+++ src/indi/ycl/model/TreeNode.java
@@ -0,0 +1,9 @@
+package indi.ycl.model;
+
+public class TreeNode {
+	 int val;
+	 TreeNode left;
+	 TreeNode right;
+	 TreeNode(int x) { val = x; }
+
+}
