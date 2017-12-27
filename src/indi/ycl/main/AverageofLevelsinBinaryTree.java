7db486192e66ffe8df0465f0e83e9c91e7b10b48
hupochuan <1434296509@qq.com>
2017-12-27 10:55:29 +0800

    1.二叉树层次遍历，用队列实现比较好； 2.关于int类型能表示的最大数 -2147483648~2147483647，超过限制只能换一种形式表示

--- src/indi/ycl/main/AverageofLevelsinBinaryTree.java
+++ src/indi/ycl/main/AverageofLevelsinBinaryTree.java
@@ -3,17 +3,19 @@ package indi.ycl.main;
 import java.util.ArrayList;
 import java.util.List;
 
+import indi.ycl.model.TreeNode;
+
 public class AverageofLevelsinBinaryTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TreeNode root=new TreeNode(2147483647);
-		root.left=new TreeNode(2147483647);
-		root.right=new TreeNode(2147483647);
+		TreeNode root=new TreeNode(1);
+		root.left=new TreeNode(2);
+		root.right=new TreeNode(3);
 		new AverageofLevelsinBinaryTree().averageOfLevels(root);
 
 	}
-
+    //用队列来做比较方便
 	public List<Double> averageOfLevels(TreeNode root) {
 		List<Double> result=new ArrayList<Double>();
 		List<TreeNode> nowlevel=new ArrayList<TreeNode>();
