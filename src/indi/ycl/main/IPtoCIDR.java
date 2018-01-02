1e753b73a80d676625577d710b29e0341e81d112
hupochuan <1434296509@qq.com>
2018-01-02 11:08:01 +0800

    较简单

---
+++ src/indi/ycl/main/IPtoCIDR.java
@@ -0,0 +1,14 @@
+package indi.ycl.main;
+
+public class IPtoCIDR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<String> ipToCIDR(String ip, int n) {
+
+	}
+
+}
